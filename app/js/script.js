453d7d6fa5b2042d92aa3926d76d3d83db8d646c
Israel Adefidipe <israeladefidipe@gmail.com>
2021-05-22 23:57:15 +0100

    added overlay styling, to appear when hamburger is clicked

--- app/js/script.js
+++ app/js/script.js
@@ -3,6 +3,7 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 
+// litens for clich on the hamburger menu
 btnHamburger.addEventListener('click', function(){
     if(header.classList.contains('open')){
         header.classList.toggle('open');
