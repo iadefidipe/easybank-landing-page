33bb9bf285e202cef25e401ee88ae7a9211abd02
Israel Adefidipe <israeladefidipe@gmail.com>
2021-05-23 03:12:20 +0100

    hero section 50% done

--- app/js/script.js
+++ app/js/script.js
@@ -2,14 +2,19 @@
 
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 // litens for clich on the hamburger menu
 btnHamburger.addEventListener('click', function(){
     if(header.classList.contains('open')){
         header.classList.toggle('open');
+        overlay.classList.add('fadeout');
     }
     else{
         header.classList.toggle('open');
+        overlay.classList.remove('fadeout');
+
+
     }
 
 })
